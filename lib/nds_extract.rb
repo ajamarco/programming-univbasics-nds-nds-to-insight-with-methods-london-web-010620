cca87281410b7656836c3a694368a6a91d6a3474
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-05 13:02:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,7 +14,7 @@ end
 # using director_data as input
 def gross_for_director(director_data)
   director_data[:movies].length.times do |movies_index|
-    puts director_data[:movies][movies_index][:]
+    puts director_data[:movies][movies_index][:worldwide_gross]
   end
   
 end
